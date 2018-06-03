b255e5857ce36ac631b3722bccb37553ae021226
xLeDocteurx <lenoirc.fr@gmail.com>
2018-06-03 14:39:52 +0200

    I commented the code and fixed the last bug I could find. Forms are working perfectly and as expected. I'm pretty sure

--- main.js
+++ main.js
@@ -2,35 +2,33 @@
 var myDatas = [];
 var myJSON = "JSON/save.json";
 
-// var regex = /^(?=.\d)(?=.[a-z])(?=.*[A-Z]).{8,16}$/g;
-// var regex = ^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,24}$;
-// var regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,24}$/g;
-
-// console.log(localStorage.getItem("json").length);
+// Si l'utisateur n'a jamais visité le site ( absence d'un local storage correspondant )
+// Nous initialisons une base de donnés en local storage a partir d'un fichier JSON
 if (localStorage.getItem("json") == null) {
     console.log(`Aucun LocalStorage "json" trouvé. nous allons le créer`);
     requestJSON(myJSON);
 
     console.log(myDatas);
     console.log(localStorage.getItem("json"));
+// Sinon nous allons simplement récupérer le local storage pour les utilisateurs et les posts.
 } else {
-    // console.log(`LocalStorage trouvé !!!`);
-    // console.log(`LocalStorage trouvé : // ${typeof JSON.parse(localStorage.getItem("json"))} // "json" trouvé.`);
-    // console.log(`LocalStorage trouvé : // ${localStorage.getItem("json").split(",")} // "json" trouvé.`);
+    
     getLocalStorage();
     getLocalPosts();
 }
 
+
+// ici nous vérifion si l'utilisateur est connecté pour afficher différents contenus et controles.
+// Les utilisateurs connectés n'ont pas besoins d'avoir accès a la page de login ni a celle d'inscription.
+// Mais ont accès au poste de... posts !
 isUserConnected();
 
-// if (localStorage.getItem("currentUser") != null) {
 
-//     document.getElementById("");
-// }
 
-// reader.addEventListener("load", function() {
-//     pre.textContent = JSON.stringify(reader.result, null, 2);
-//   });
+/////////////////////////////////////////////////////////////////////////////
+// Fonctions concernant la vérification des formulaire et la publication // modification concernant la gestions des blogs posts
+/////////////////////////////////////////////////////////////////////////////
+// vérification
 function checkIfPostIsOk() {
 
     var form = document.forms.namedItem("user-form");
@@ -41,7 +39,28 @@ function checkIfPostIsOk() {
 
     registerNewPost(title, content, author);
 }
+// création
+function registerPostContent(id) {
 
+    myDatas.posts[id].content = document.getElementById(`new-post-content_${id}`).value;
+    console.log(document.getElementById(`new-post-content_${id}`).value);
+    // console.log(myDatas.posts[id].content);
+    localStorage.setItem("json", JSON.stringify(myDatas));
+    backHomeMarty();
+    console.log("user is trying to save a post. post : " + id);
+}
+// vérification d'édition
+function checkIfEditIsOk() {
+
+    var form = document.forms.namedItem("user-form");
+
+    var title = form.title.value;
+    var content = form.content.value;
+    var author = JSON.parse(localStorage.getItem("currentUser")).username;
+
+    registerNewPost(title, content, author);
+}
+// édition
 function editPost(id) {
 
     console.log("user istrying to edit post n°" + id);
@@ -64,17 +83,20 @@ function editPost(id) {
     // actualpostcontent.innerHTML = ``;
 
 }
+// enregistrement du post
+function registerNewPost(title, content, author) {
 
-function registerPostContent(id) {
+    if (title != "" && content != "") {
 
-    myDatas.posts[id].content = document.getElementById(`new-post-content_${id}`).value;
-    console.log(document.getElementById(`new-post-content_${id}`).value);
-    // console.log(myDatas.posts[id].content);
-    localStorage.setItem("json", JSON.stringify(myDatas));
-    backHomeMarty();
-    console.log("user is trying to save a post. post : " + id);
+        var newPost = new Post(title, content, author);
+        myDatas = JSON.parse(localStorage.getItem("json"));
+        myDatas.posts.push(newPost);
+        localStorage.setItem("json", JSON.stringify(myDatas));
+
+        backHomeMarty();
+    }
 }
-
+// suppréssion
 function deletePost(id) {
 
     myDatas = JSON.parse(localStorage.getItem("json"));
@@ -90,17 +112,11 @@ function deletePost(id) {
     backHomeMarty();
 }
 
-function checkIfEditIsOk() {
 
-    var form = document.forms.namedItem("user-form");
-
-    var title = form.title.value;
-    var content = form.content.value;
-    var author = JSON.parse(localStorage.getItem("currentUser")).username;
-
-    registerNewPost(title, content, author);
-}
-
+/////////////////////////////////////////////////////////////////////////////
+// Fonctions concernant la vérification des formulaire et la publication // modification concernant la gestions des utilisateurs
+/////////////////////////////////////////////////////////////////////////////
+// vérification empechant les doublons d'usernames
 function checkUsername(that) {
     var input = that.value;
 
@@ -116,19 +132,15 @@ function checkUsername(that) {
         inputField.setAttribute("class", "form-control is-invalid");
     }
 }
-
+//vérification du formulaire
 function checkIfFormIsOk() {
+    var form = document.forms.namedItem("user-form");
 
-    if (document.getElementsByClassName("invalid")[0]) {
+    if (form.checkValidity()) {
+    console.log("si la fonction s'éxécute");
         
-        console.log(document.getElementsByClassName("invalid")[0]);
-
-        var form = document.forms.namedItem("user-form");
-
         var email = form.email.value;
-        console.log(form.password);
         form.password.type = "text";
-        console.log(form.password);
         var password = form.password.value;
         var x = form.x.checked;
         var y = form.y.checked;
@@ -151,26 +163,15 @@ function checkIfFormIsOk() {
         }
 
         registerNewUser(username, password, firstname, lastname, sex, email, phone, address, city, website, color, hobbies);
+    } else {
+        console.log("Il y a des classes invalides //");
     }
 }
-
-function registerNewPost(title, content, author) {
-
-    if (title != "" && content != "") {
-
-        var newPost = new Post(title, content, author);
-        myDatas = JSON.parse(localStorage.getItem("json"));
-        myDatas.posts.push(newPost);
-        localStorage.setItem("json", JSON.stringify(myDatas));
-
-        backHomeMarty();
-    }
-}
-
+// We need to go back to the future marty !
 function backHomeMarty() {
     window.location.href = "index.html";
 }
-
+// enregistrement de l'utilisateur
 function registerNewUser(username, password, firstname, lastname, sex, email, phonenumber, adress, city, website, color, hobbies) {
 
     var currentUser = new User(username, password, firstname, lastname, sex, email, phonenumber, adress, city, website, color, hobbies);
@@ -180,15 +181,15 @@ function registerNewUser(username, password, firstname, lastname, sex, email, ph
     console.log("mydatas : ");
     console.log(myDatas);
     localStorage.setItem("json", JSON.stringify(myDatas));
-    console.log("localstorage : ");
-    console.log(localStorage.getItem("json"));
+    // console.log("localstorage : ");
+    // console.log(localStorage.getItem("json"));
     getLocalStorage();
 
     localStorage.setItem("currentUser", JSON.stringify(currentUser));
 
-    // backHomeMarty();
+    backHomeMarty();
 }
-
+// Log in de l'utilisateur
 function logIn() {
 
     var form = document.forms.namedItem("signin-form");
@@ -204,20 +205,13 @@ function logIn() {
     var found = myDatas.users.find(element => {
         return element.username == username;
     });
-    // console.log("username in array :");
-    // console.log(found);
 
-
+//Si il y a correspondance entre l'username entré et un username de la base de donné
+// l'algorythme compare le mot de passe en entré et celui de l'utilisateur trouvé dans la base de donné
     if (found != null) {
-        // console.log("found got found : ");
-        // console.log(found);
-        // var indexOfUsername = myDatas.users.indexOf(found);
-        // console.log("index of username : ");
-        // console.log(indexOfUsername);
         console.log("console.log(myDatas.users.indexOf(found));");
         console.log(myDatas.users.indexOf(found));
 
-        // if (myDatas.users.indexOf(found) > 0) {
         var indexOfUsername = myDatas.users.indexOf(found);
         console.log("index of username : ");
         console.log(indexOfUsername);
@@ -227,28 +221,18 @@ function logIn() {
             if (password == myDatas.users[indexOfUsername].password) {
                 var user = myDatas.users[indexOfUsername];
                 localStorage.setItem("currentUser", JSON.stringify(user));
-
-                //                           /////////////////////
-
-
-
             } else {
                 localStorage.removeItem("currentUser");
                 alert("Wrong password");
             }
         }
-        // }
     } else {
         localStorage.removeItem("currentUser");
         alert("We don't know this user");
     }
 }
 
-// function getCurrentUser() {
-//     var user = JSON.parse(localStorage.getItem("currentUser"));
-//     return user;
-// }
-
+// Si l'utilisateur est connecté nous intervenons sur le dom pour afficher un contenu différent
 function isUserConnected() {
 
     if (localStorage.getItem("currentUser") != null) {
@@ -280,6 +264,7 @@ function logOut() {
     window.location.href = "index.html";
 }
 
+// Afficher dans la page un seul utilisateur ( profil )
 function oneUser(user) {
     // console.log(local);
     // console.log(local.stringify(myDatas));
@@ -307,6 +292,7 @@ function oneUser(user) {
     // }
 }
 
+// afficher tous les posts de la base de donnés
 function getLocalPosts() {
     var local = JSON.parse(localStorage.getItem("json"));
     // console.log(local);
@@ -353,6 +339,7 @@ function getLocalPosts() {
     }
 }
 
+// récupération de la base de donnés dans le local storage
 function getLocalStorage() {
     var local = JSON.parse(localStorage.getItem("json"));
     // console.log(local);
